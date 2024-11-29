8bc2a9c765cf4edea007482713bfd8bc211838a0
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2024-11-29 20:16:35 +0700

    feat: clicking logo now takes you back to home

--- src/components/fragments/Logo.jsx
+++ src/components/fragments/Logo.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 export default function Logo() {
+  function handleLogoClick() {
+    window.location.href = "/home";
+  }
   return (
-    <h1 class="most-wazted logo text-3xl text-center">
+    <h1
+      class="most-wazted logo text-3xl text-center cursor-pointer"
+      onClick={handleLogoClick}
+    >
       Sneaker
       <br />
       Scout
