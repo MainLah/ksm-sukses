2f416df6f0d861df0bbeb48d659b6daa442f213d
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2024-11-30 14:57:37 +0700

    table color finished

--- src/components/fragments/ColorBubble.jsx
+++ src/components/fragments/ColorBubble.jsx
@@ -2,5 +2,10 @@ import React from "react";
 
 export default function ColorBubble(props) {
   const { bgColor } = props;
-  return <div className={"w-3 rounded-full " + { bgColor }}>asdas </div>;
+  return (
+    <div
+      className="size-8 rounded-full my-1 border border-black"
+      style={{ backgroundColor: `#${bgColor}` }}
+    ></div>
+  );
 }
--- src/components/fragments/TableColor.jsx
+++ src/components/fragments/TableColor.jsx
@@ -3,41 +3,36 @@ import ColorBubble from "./ColorBubble";
 
 export default function TableColor() {
   const colorGridData = {
-    "#000000": 7,
-    "#FF0000": 7,
-    "#FEFDFF": 7,
-    "#B8AA91": 7,
-    "#FFA300": 7,
-    "#33FF00": 7,
-    "#00A3FF": 7,
+    "000000": 7,
+    FF0000: 7,
+    FEFDFF: 7,
+    B8AA91: 7,
+    FFA300: 7,
+    "33FF00": 7,
+    "00A3FF": 7,
   };
 
+  // const colorGridData = [{"#000000": 7}]
+
   function renderBubble() {
-    for (const { color, key } in colorGridData) {
-      return <ColorBubble color={color} key={key} />;
-    }
+    return Object.keys(colorGridData).map((key) => (
+      <div>{Array(7).fill(<ColorBubble key={key} bgColor={key} />)}</div>
+    ));
   }
 
   return (
-    // <table className="border-[1px] w-72">
-    //   <tr>
-    //     <th className="font-normal border-b-[1px]">
-    //       <p className="text-left p-5 text-2xl">Color</p>
-    //     </th>
-    //   </tr>
-    //   <tr className="flex flex-col p-5">
-    //     <div className="grid">
-    //       <ColorBubble color="#fefdff"></ColorBubble>
-    //     </div>
-    //   </tr>
-    // </table>
-    <div>
-      <div>
-        <p>Color</p>
-      </div>
-      <div>
-        <ColorBubble bgColor="bg-[#00a3ff]"></ColorBubble>
-      </div>
-    </div>
+    <table className="border w-72">
+      <tr>
+        <th className="font-normal border-b">
+          <p className="text-left p-5 text-2xl">Color</p>
+        </th>
+      </tr>
+      <tr className="flex flex-col p-5">
+        <div className="flex justify-between">
+          {/* <ColorBubble bgColor="000000"></ColorBubble> */}
+          {renderBubble()}
+        </div>
+      </tr>
+    </table>
   );
 }
