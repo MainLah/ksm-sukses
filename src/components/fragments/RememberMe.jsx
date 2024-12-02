8df202424b1b434769854436331e44957dceb59e
Rendra Kusuma Blikololo <rendrakusumab@gmail.com>
2024-12-02 09:48:08 +0700

    minor changes to style

--- src/components/fragments/RememberMe.jsx
+++ src/components/fragments/RememberMe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function rememberMe() {
   return (
-    <div className="below-inputs flex justify-between my-[1rem]">
+    <div className="below-inputs flex justify-between mt-5 mb-2">
       <div className="remember-me-div">
         <input type="checkbox" name="remember-me" className="mr-2" />
         <label htmlFor="remember-me">Remember me</label>
